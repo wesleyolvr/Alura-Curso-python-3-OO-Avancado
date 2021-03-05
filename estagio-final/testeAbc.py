e38efcf2886da76c487af7027a7f5bc782fa7b4f
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2021-03-05 15:37:56 -0300

    arquivos do curso

---
+++ estagio-final/testeAbc.py
@@ -0,0 +1,22 @@
+from collections.abc import MutableSequence
+
+class MinhaListinhaMutavel(MutableSequence):
+    pass
+"""    def __init__(self):
+        pass
+    def __getitem__(self):
+        pass
+
+    def __len__(self):
+        pass
+    def __delitem__(self):
+        pass
+    def __setitem__(self):
+        pass
+    def insert(self):
+        pass"""
+
+
+
+objetoValidado = MinhaListinhaMutavel()
+print(objetoValidado)
